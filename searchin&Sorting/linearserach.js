e7d620591b231da318d6e2252b59adbe4f8b2e49
vanshtyagi384 <vanshtyagi.tyagi012@gmail.com>
2026-01-05 19:38:35 +0530

    Linearserachupdated

--- searchin&Sorting/linearserach.js
+++ searchin&Sorting/linearserach.js
@@ -1,13 +1,12 @@
 let arr = [3,4,5,6,4,3,2];
-
-function linerSearch(arr, target){
+function linearSearch(arr, target){
     for(let i = 0; i< arr.length; i++ ){
         if(arr[i]=== target){
-            return i; 
+            return i;
         }
     }
     return -1;
 }
 
 
-console.log(linerSearch(arr, 64));
+console.log(linearSearch(arr,4));
