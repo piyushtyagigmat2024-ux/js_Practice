23c59bbc4c20159c1790b623459d050f118d4a27
vanshtyagi384 <vanshtyagi.tyagi012@gmail.com>
2026-01-06 18:10:26 +0530

    linklistfile

---
+++ linklist/LinkList.js
@@ -0,0 +1,13 @@
+class Node{
+    constructor(data){
+        this.value = data; 
+        this.next = null;
+    }
+}
+
+class LinkList{
+    constructor(){
+        this.head = null; 
+        this.count=0;
+    }
+}
